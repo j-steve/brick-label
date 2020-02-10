64ae75636e0de5b8d79e659803f1cd337f4fbae6
Steve <steve5805@gmail.com>
2020-02-10 20:21:43 +0000

    Imports initial project.

---
+++ colors.js
@@ -0,0 +1,13 @@
+const COLOR = {
+  blue: '#0055BF',
+  brightGreen: '#4B9F4A',
+  red: '#C91A09',
+  yellow: '#F2CD37',
+  lightBluishGray: '#A0A5A9',
+  darkBluishGray: '#6c6e68',
+  lightGray: '#9BA19D',
+  black: '#05131D',
+  white:'#fff',
+  green: '#237841',
+  mediumDarkFlesh: '#cc702a',
+};
