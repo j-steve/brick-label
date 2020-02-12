0694d0c96c36b355ac1237071cfe2c1b35b79f74
Steve <steve5805@gmail.com>
2020-02-12 22:20:12 +0000

    Added more bricks (BRICKS3) and minor style adjustments.

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const BRICKLINK = {
 const TITLE_REGEX = /^(?:(?<d1>\d+) x (?<d2>\d+)(?: x (?<d3>\d+))? ?)?(?<title>[\w  ]+)?/;
 const SUBTITLE_REGEX = /^(?<title>[\w ]+?)(?: with (?<subtitle>[\w ]+))?$/;
 
-LEGOS2.forEach(createLabel);
+LEGOS3.forEach(createLabel);
 
 function createLabel(part) {
   getBricklinkHtml(part.id).then(([partTitle, category]) => {
@@ -21,8 +21,8 @@ function createLabel(part) {
       .replace(/ x /g, '×')
       .replace(/ 1\/3/g, '⅓')
       .replace('2/3', '⅔')
-      .replace(/^45 /g, '')
-      .replace(/^30 /g, ''); 
+      .replace(/^\d\d /g, '')
+      .replace(' - ', ' ');
     let withSplitSubTitle;
     [part.title, withSplitSubTitle] = part.title.split(' with ');
     if (!part.subtitle && withSplitSubTitle) {
@@ -35,6 +35,7 @@ function createLabel(part) {
         part.subtitle = splitParts.join(' ');
       }
     }
+    if (part.category == 'Cone') {part.category = 'Brick, Cone';}
     part.isTitleNumeric = part.title.includes('×');
     part.size = part.size || Math.max(...part.title.split('×')) || '';
     part.imgPath = part.imagePath || `https://img.bricklink.com/ItemImage/PN/${part.imageColor || 11}/${part.id}.png`
@@ -79,9 +80,10 @@ function buildLabelElement(part) {
   }
 
   const useNumericFont = part.isTitleNumeric && part.title.split('×').length < 3;
-  $('<div>').addClass('part-title').toggleClass('title-numeric', useNumericFont)
-    .text(part.title)
-    .appendTo($container);
+  const $title = $('<div>').addClass('part-title')
+    .toggleClass('title-numeric', useNumericFont)
+    .text(part.title).appendTo($container);
+  if (part.titleSize) {$title.css('font-size', part.titleSize);}
   $('<div>').addClass('part-subtitle').text(part.subtitle).appendTo($container);
   if (part.color || part.colorTranslucent || part.color1 && part.color2) {
     $color = $('<div>').addClass('color')
@@ -91,7 +93,7 @@ function buildLabelElement(part) {
     if (part.color) {
       $colorInner.css('background-color', part.color);
     } else if (part.color1 && part.color2) {
-      const gradiant = `linear-gradient(to bottom right, ${part.color1} 0%, ${part.color1} 44%, ${part.color2} 46%, ${part.color2} 100%)`;
+      const gradiant = `linear-gradient(to bottom right, ${part.color1} 0%, ${part.color1} 49%, ${part.color2} 51%, ${part.color2} 100%)`;
       $colorInner.css('background', gradiant);
     } else {
       $colorInner.addClass('rainbow');
